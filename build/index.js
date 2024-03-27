2dbfdd0d9e483c6385d2263b5d2b949bdb983772
ASIAPAC\FENGT <fengt@airproducts.com>
2024-03-27 15:46:24 +0800

    Add the support for Typescript

--- build/index.js
+++ build/index.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
 console.log("Starting  frontend-cli");
 const world = 'world';
-function hello(who = world) {
+export function hello(who = world) {
     console.log(`Hello ${who}! `);
     return `Hello ${who}! `;
 }
-exports.hello = hello;
 console.log("End of  frontend-cli");
