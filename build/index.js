7a2c862d9302cff4613c095b68295ef93ff8cf64
love life <47026173+273299007@users.noreply.github.com>
2024-03-27 15:12:42 +0800

    Add bin to package.json (#1)
    Co-authored-by: Will Feng

--- build/index.js
+++ build/index.js
@@ -2,10 +2,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.hello = void 0;
-console.log("Awesome Command");
+console.log("Starting  frontend-cli");
 const world = 'world';
 function hello(who = world) {
     console.log(`Hello ${who}! `);
     return `Hello ${who}! `;
 }
 exports.hello = hello;
+console.log("End of  frontend-cli");
