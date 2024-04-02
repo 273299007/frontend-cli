9e9b5545686ea00e27ba18b09a8eae849b7669b7
ASIAPAC\FENGT <fengt@airproducts.com>
2024-04-02 09:49:22 +0800

    fix the package.json error

--- build/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-console.log("Starting  frontend-cli");
-const world = 'world';
-export function hello(who = world) {
-    console.log(`Hello ${who}! `);
-    return `Hello ${who}! `;
-}
-console.log("End of  frontend-cli");
